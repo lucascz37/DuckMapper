314de0fe474ed93cbbd9ae9b2633d276b94f3729
Lucas A <la0359813@gmail.com>
2021-07-04 15:46:06 -0300

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="DuckMapper",
     version="1.0.0",
@@ -7,6 +10,8 @@ setup(
     author_email="luke.andrade99@gmail.com",
     packages=["DuckMapper"],
     description="A tiny library to help you to convert classes to DTO and vice versa.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/lucascz37/DuckMapper",
     license="MIT",
     keywords="DTO conversor mapper classMapper DuckMapper",
